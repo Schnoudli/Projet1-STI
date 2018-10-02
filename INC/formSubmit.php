d8b2c02e7754d2bc6dca8f9a0c0cc6f1c0dc08ee
Andre <andre.jacquemond@heig-vd.ch>
2018-10-02 16:37:26 +0200

    test si resultat et si actif ou non

--- INC/formSubmit.php
+++ INC/formSubmit.php
@@ -24,18 +24,24 @@ echo "Database loaded successfully ...." . "<br/>";
 
 $name = $_POST["login"];
 $password = $_POST["password"];
-echo "name " . $name . "pass " . $password;
+echo "name " . $name . "pass " . $password  . "<br/>";;
 $result = $file_db->query("SELECT Actif, Username, Pass, Admin FROM Personne WHERE Username='$name' AND Pass='$password'");
-if($result != NULL){
+if($result === NULL){
+    echo "Login impossible, aller prendre un café avec l'admin pour obtenir un compte";
+}
+else{
     foreach ($result as $row) {
         echo "Actif: " . $row['Actif'] . "<br/>";
         echo "Username: " . $row['Username'] . "<br/>";
         echo "Pass: " . $row['Pass'] . "<br/>";
         echo "Admin: " . $row['Admin'] . "<br/>";
         echo "<br/>";
+        if(empty($row['Actif'])){
+            echo "Compte inactif!" . "<br/>";
+        }
+        else{
+            echo "lancement de page messages" . "<br/>";
+        }
     }
-}
-else{
-    echo "Login impossible, aller prendre un café avec l'admin pour obtenir un compte";
 }
 ?>
