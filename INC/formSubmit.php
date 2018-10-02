e4ab469a3d2946cc81a1e9a282a4e3b43b239420
Andre <andre.jacquemond@heig-vd.ch>
2018-10-02 15:35:29 +0200

    Recherche dans la db par username + pass OK

--- INC/formSubmit.php
+++ INC/formSubmit.php
@@ -5,8 +5,37 @@
  * Date: 02/10/2018
  * Time: 08:54
  */
-    $db = new SQLite3( "sqlite", SQLITE3_OPEN_READWRITE);
-    $name = $_POST["login"];
-    $password = $_POST["password"];
-    $result = $db->query("SELECT login FROM ????");
+try
+{
+    /*** connect to SQLite database ***/
 
+    $file_db = new PDO("sqlite:/usr/share/nginx/databases/database.sqlite");
+    echo "Handle has been created ...... <br><br>";
+
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+    echo "<br><br>Database -- NOT -- loaded successfully .. ";
+    die( "<br><br>Query Closed !!! $error");
+}
+
+echo "Database loaded successfully ...." . "<br/>";
+
+$name = $_POST["login"];
+$password = $_POST["password"];
+echo "name " . $name . "pass " . $password;
+$result = $file_db->query("SELECT Actif, Username, Pass, Admin FROM Personne WHERE Username='$name' AND Pass='$password'");
+if($result != NULL){
+    foreach ($result as $row) {
+        echo "Actif: " . $row['Actif'] . "<br/>";
+        echo "Username: " . $row['Username'] . "<br/>";
+        echo "Pass: " . $row['Pass'] . "<br/>";
+        echo "Admin: " . $row['Admin'] . "<br/>";
+        echo "<br/>";
+    }
+}
+else{
+    echo "Login impossible, aller prendre un café avec l'admin pour obtenir un compte";
+}
+?>
